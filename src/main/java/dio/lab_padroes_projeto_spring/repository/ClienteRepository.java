e720d8dc694f0e8671c06b0812f15a58cc441502
diana <dianab_silvaa@hotmail.com>
2024-07-29 18:40:29 -0300

    Resolução de desafio

--- src/main/java/dio/lab_padroes_projeto_spring/repository/ClienteRepository.java
+++ src/main/java/dio/lab_padroes_projeto_spring/repository/ClienteRepository.java
@@ -1,9 +1,10 @@
-package one.digitalinnovation.gof.model;
+package dio.lab_padroes_projeto_spring.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import dio.lab_padroes_projeto_spring.model.Cliente;
+
 @Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
-
 }
--- src/main/java/dio/lab_padroes_projeto_spring/repository/EnderecoRepository.java
+++ src/main/java/dio/lab_padroes_projeto_spring/repository/EnderecoRepository.java
@@ -1,9 +1,10 @@
-package one.digitalinnovation.gof.model;
+package dio.lab_padroes_projeto_spring.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import dio.lab_padroes_projeto_spring.model.Endereco;
+
 @Repository
 public interface EnderecoRepository extends CrudRepository<Endereco, String> {
-
 }
